42fb83581ccb2ebb92e7720384cb85d35d80dd54
Christopher Gray <Christopher Gray>
2017-03-26 15:20:09 -0500

    mute before powering off

--- piDeskServer.js
+++ piDeskServer.js
@@ -4,25 +4,41 @@ const gpio = require('rpi-gpio')
 const wss = new WebSocket.Server({ port: 8080 });
 
 var power = false;
+var mute = false;
+
+const pinConfig = {
+    power: {
+        number: 18,
+        direction: gpio.DIR_OUT
+    },
+    mute: {
+        number: 17,
+        direction: gpio.DIR_OUT
+    },
+}
 
 wss.on('connection', function connection(ws) {
     gpio.setMode(gpio.MODE_BCM);
+    gpio.setup(pinConfig.power.number, pinConfig.power.direction);
+    gpio.setup(pinConfig.mute.number, pinConfig.mute.direction);
 
     ws.on('message', function incoming(message) {
         if (message == 'powerToggle') {
             power = !power;
-            
-            gpio.setup(18, gpio.DIR_OUT, write);
 
-            function write() {
-                gpio.write(18, power, function (err) {
-                    if (err) throw err;
-                    console.log('Written to pin 18');
-                });
-            }
+            writePin(pinConfig.mute.number, true);
+            writePin(pinConfig.power.number, power);
         }
 
     });
 
     ws.send('something');
 });
+
+
+function writePin(pinNumber, value) {
+    gpio.write(pinNumber, value, function (err) {
+        if (err) throw err;
+        console.log('Written to pin ' + pinNumber);
+    });
+}
