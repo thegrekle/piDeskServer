482963e254bb55492eabdd4d231c22930cc57d0a
thegrekle <thegrekle@thegrekles.net>
2017-03-26 15:11:51 -0500

    Fix gpio reference

--- piDeskServer.js
+++ piDeskServer.js
@@ -6,16 +6,16 @@ const wss = new WebSocket.Server({ port: 8080 });
 var power = false;
 
 wss.on('connection', function connection(ws) {
-    this.gpio.setMode(this.gpio.MODE_BCM);
+    gpio.setMode(gpio.MODE_BCM);
 
     ws.on('message', function incoming(message) {
         if (message == 'powerToggle') {
-            this.power = !this.power;
+            power = !power;
             
-            this.gpio.setup(18, this.gpio.DIR_OUT, write);
+            gpio.setup(18, gpio.DIR_OUT, write);
 
             function write() {
-                this.gpio.write(18, this.power, function (err) {
+                gpio.write(18, power, function (err) {
                     if (err) throw err;
                     console.log('Written to pin 18');
                 });
