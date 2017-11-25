2bab453d5536bab1d9d6336a173d3a368cb7f271
Christopher Gray <chris@lastcontact.us>
2017-11-24 20:37:45 -0600

    Clean up some variable names

--- piDeskServer.js
+++ piDeskServer.js
@@ -5,7 +5,7 @@ var async = require('async');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-var power = false;
+var powerState = false;
 var mustState = false;
 
 const pinConfig = {
@@ -38,29 +38,14 @@ wss.on('connection', function connection(ws) {
         }
     ], function (err, results) {
         console.log('Pins set up');
-        async.series([
-            function (callback) {
-                delayedWrite(pinConfig.mute, true, callback);
-            },
-            function (callback) {
-                delayedWrite(pinConfig.power, power, callback, 0);
-            }
-        ], function (err, results) {
-            if (err) {
-                console.error(err);
-            }
-            else {
-                console.log(`Powered ${power ? "on" : "off"}`);
-            }
-        });
     });
 
     ws.on('message', function incoming(message) {
         if (message == 'powerToggle') {
-            power = !power;
-            delayedWrite(pinConfig.mute, power, function () { }, 0);
-            delayedWrite(pinConfig.power, power, function () { }, 250);
-            delayedWrite(pinConfig.mute, !power, function () { }, 250);
+            powerState = !powerState;
+            delayedWrite(pinConfig.mute, powerState, function () { }, 0);
+            delayedWrite(pinConfig.power, powerState, function () { }, 250);
+            delayedWrite(pinConfig.mute, !powerState, function () { }, 250);
         }
 
         if (message == 'muteToggle') {
@@ -103,11 +88,11 @@ function beginSpi() {
 }
 
 function broadcastPowerState() {
-    wss.broadcast(`powerState:${power}`);
+    wss.broadcast(`powerState:${powerState}`);
 }
 
 function broadcastMuteState() {
-    wss.broadcast(`muteState:${mute}`);
+    wss.broadcast(`muteState:${muteState}`);
 }
 
 function broadcastVolume() {
